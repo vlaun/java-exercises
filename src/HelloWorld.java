61f636da92bf948fc09093d44627febf61f88924
Yleana Vlaun <cyvlaun4@gmail.com>
2016-11-11 11:43:55 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * Created by yleanavlaun on 11/11/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args){//this will be the same for every program for the next 2 weeks
+//        window.console.log()
+
+        System.out.print("Hello world!!"); //prints the line
+        System.out.println("Hello world!!");//prints out the line
+        System.out.printf("\nHello %s, today is %s, and its the %dth day of the week", "Codeup", "Friday", 5);
+                          // \n gives you new line
+        //when you run the command the result is....
+        //HellosCodeup, today is Friday, and its the 5th day of the week
+        //Process finished with exit code 0
+
+    }
+}
+
+// or you can use psvm instead of writing out the whole command of public static void main and you can
+//also use sout instead of writing out System.out.print
