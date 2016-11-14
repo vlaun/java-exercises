e40475bb4c71f5023a40a4bb1d149c8b498a226a
Yleana Vlaun <cyvlaun4@gmail.com>
2016-11-14 13:47:08 -0600

    Strings

---
+++ src/Strings.java
@@ -0,0 +1,19 @@
+/**
+ * Created by yleanavlaun on 11/14/16.
+ */
+public class Strings {
+    public static void main(String[] args) {
+        String message = "We don't need no education. " + "We don't need no thought control";
+        System.out.println("We don't need no education. " + "We don't need no thought control");
+
+        String message2 = "Check \"this\" out!, " + "\"s inside of \"s!";
+        System.out.println("Check \"this\" out!,"  + "\"s inside of \"s!");
+
+        String message3 = "In windows, the main drive is usally C:\\";
+        System.out.println("In windows, the main drive is usally C:\\");
+
+        String message4 = "I can do back slashes '\\', and double back slashes '\\\\', and the amazing triple back-s";
+        System.out.println("I can do back slashes '\\', and double back slashes '\\\\', and the amazing triple back-s");
+
+    }
+}
